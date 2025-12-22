fe4bd2936c2d2140f2d4d31eae471ff047bd3961
V-vidit <viditkhant@gmail.com>
2025-12-22 21:24:02 +0530

    CRUD

--- Lab-9_10/routes/userRoutes.js
+++ Lab-9_10/routes/userRoutes.js
@@ -1,25 +1,52 @@
-const express= require("express");
-const router=express.Router();
-const User=require("../models/user");
-const authMiddleware= require("../middlewares/authMiddleware");
+const express = require("express");
+const router= express.Router();
 
-router.get("/",authMiddleware,async(req,res)=>{
-    const user= await User.find();
-    return res.status(200).json({users: user});
+const mongoose=require("mongoose");
+const User= require("../models/user");
+
+
+router.get("/", async(req,res)=>{
+    const users=await User.find();
+    return res.status(200).json({users: users});
 })
 
 router.post("/register", async(req,res)=>{
-    const user=new User({
-        name:"Vishu",
-        email: "vishu@gmail.com",
-        password: "12345678",
-        phone: 9999988888
+    const {name,email, password, phone}= req.body;
+
+    const user=await User.create({
+        name,
+        email,
+        password,
+        phone
     });
 
-    const newUser = await user.save();
+    return res.status(200).json({message: "User registered successfully", user: user});
+})
 
-    return res.status(200).json({message: "User registered successfully", user: newUser})
+router.patch("/update/:id", async(req,res)=>{
+    const user= await User.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set: req.body,
+        },
+        {new: true}
+    );
 
+    res.json({method: "Patch", updatedUser:user});
+}) 
+
+
+router.delete("/delete/:id", async(req,res)=>{
+    const deletedUser= await User.findByIdAndDelete(req.params.id);
+    
+    if(!deletedUser){
+        return res.status(404).json({message: "User not found"});
+    }
+
+    res.json({
+        message: "User deleted successfully",
+        user: deletedUser
+    })
 })
 
 
