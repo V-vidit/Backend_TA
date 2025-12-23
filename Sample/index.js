893722f90492869c5e99f5bb907b8071aaf99b72
V-vidit <viditkhant@gmail.com>
2025-12-23 19:06:08 +0530

    CRUD

--- Sample/index.js
+++ Sample/index.js
@@ -1,60 +1,81 @@
-const express= require("express");
-const mongoose= require("mongoose");
-const dotenv=require("dotenv");
+const express=require("express");
+const mongoose=require("mongoose");
 const app=express();
-dotenv.config()
-const User=require("./models/user")
-const bcrypt=require("bcryptjs")
+
+const dotenv=require("dotenv");
+dotenv.config();
 
 mongoose.connect(process.env.mongourl).then(()=>{
     console.log("DB Connected");
 }).catch((err)=>{
     console.log(err);
-})
+});
 
-app.use(express.json())
+const User=require("./models/user");
 
+app.use(express.json());
+// GetAll users route
 app.get("/users", async(req,res)=>{
     try{
-        const users=await User.find();
-        res.json({users: users})
+        const users= await User.find();
+        res.json({message: "Users fetched successfully", allUsers: users})
     }
     catch(err){
         res.json({err});
     }
 })
 
-app.post("/register", async(req,res)=>{
+app.get("/users/:id", async(req,res)=>{
     try{
-        const {name,email,password,phone}= req.body;
+        const user=await User.findById(req.params.id);
 
-        const hashedPassword= await bcrypt.hash(password,10);
+        if (!user) {
+            return res.json({message: "User not found"})
+        }
 
-        const user=await User.create({
-            name,
-            email,
-            password: hashedPassword,
-            phone
-        })
-
-        res.json({message: "User created successfully", user: user});
+        res.json({mesaage: "User fetched", user:user})
     }
     catch(err){
         res.json({err})
     }
 })
 
+//Create a user
+app.post("/register", async(req,res)=>{
+    try{
+        const {name,email,password,phone}= req.body;
+
+        const user= await User.create({
+            name,
+            email,
+            password,
+            phone
+        });
+
+        res.json({message: "User created successfully", newUser: user});
+    }
+    catch(err){
+        res.json({err});
+    }
+})
+
 app.patch("/update/:id", async(req,res)=>{
     try{
-        const user= await User.findByIdAndUpdate(
+        const users=await User.findByIdAndUpdate(
             req.params.id,
             {
                 $set: req.body
             },
-            {new:true}
+            {
+                new: true
+            }
         );
 
-        res.json({message: "User updated successfully", user: user});
+        if (!users) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        res.json({message: "User updated successfully", updatedUser: users});
     }
     catch(err){
         res.json({err});
@@ -66,7 +87,7 @@ app.delete("/delete/:id", async(req,res)=>{
         const user= await User.findByIdAndDelete(req.params.id);
 
         if (!user) {
-            return res.status(404).json({message: "User not found"});
+            return res.status(404).json({message: "User not found"})
         }
 
         res.json({message: "User deleted successfully", deletedUser: user})
